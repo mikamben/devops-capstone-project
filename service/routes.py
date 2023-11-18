2f468923ebed45de9e41ca6499fa1280a97aa39c
mikamben <lionsik@yahoo.fr>
2023-11-17 20:25:14 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -71,9 +72,8 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-    ######################################################################
 
-        
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -128,6 +128,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
